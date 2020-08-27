1a3f9e52b5712d64022a10040be9a62cd92dbf60
Amit Sagtani <asagtani06@gmail.com>
2020-08-27 20:31:26 +0530

    fixed existing linter issues
    removed the extra commas from mb.py to
    fix the code style errors.

--- src/touchstone/benchmarks/mb.py
+++ src/touchstone/benchmarks/mb.py
@@ -37,7 +37,7 @@ class Mb(BenchmarkBaseClass):
                 "metadata": {
                     "cpuinfo-metadata": {
                         "element": "pod_name",
-                        "compare": ["value.Model name", "value.Architecture", "value.CPU(s)",],
+                        "compare": ["value.Model name", "value.Architecture", "value.CPU(s)"],
                     },
                     "meminfo-metadata": {
                         "element": "pod_name",
@@ -56,33 +56,33 @@ class Mb(BenchmarkBaseClass):
                         ],
                         "compute": [
                             {
-                                "filter": {"test_type": "http",},
-                                "buckets": ["routes", "conn_per_targetroute", "keepalive",],
-                                "aggregations": {"requests_per_second": ["avg"], "latency_95pctl": ["avg"],},
+                                "filter": {"test_type": "http"},
+                                "buckets": ["routes", "conn_per_targetroute", "keepalive"],
+                                "aggregations": {"requests_per_second": ["avg"], "latency_95pctl": ["avg"]},
                                 "collate": [],
                             },
                             {
-                                "filter": {"test_type": "edge",},
-                                "buckets": ["routes", "conn_per_targetroute", "keepalive",],
-                                "aggregations": {"requests_per_second": ["avg"], "latency_95pctl": ["avg"],},
+                                "filter": {"test_type": "edge"},
+                                "buckets": ["routes", "conn_per_targetroute", "keepalive"],
+                                "aggregations": {"requests_per_second": ["avg"], "latency_95pctl": ["avg"]},
                                 "collate": [],
                             },
                             {
-                                "filter": {"test_type": "passthrough",},
-                                "buckets": ["routes", "conn_per_targetroute", "keepalive",],
-                                "aggregations": {"requests_per_second": ["avg"], "latency_95pctl": ["avg"],},
+                                "filter": {"test_type": "passthrough"},
+                                "buckets": ["routes", "conn_per_targetroute", "keepalive"],
+                                "aggregations": {"requests_per_second": ["avg"], "latency_95pctl": ["avg"]},
                                 "collate": [],
                             },
                             {
-                                "filter": {"test_type": "reencrypt",},
-                                "buckets": ["routes", "conn_per_targetroute", "keepalive",],
-                                "aggregations": {"requests_per_second": ["avg"], "latency_95pctl": ["avg"],},
+                                "filter": {"test_type": "reencrypt"},
+                                "buckets": ["routes", "conn_per_targetroute", "keepalive"],
+                                "aggregations": {"requests_per_second": ["avg"], "latency_95pctl": ["avg"]},
                                 "collate": [],
                             },
                             {
-                                "filter": {"test_type": "mix",},
-                                "buckets": ["routes", "conn_per_targetroute", "keepalive",],
-                                "aggregations": {"requests_per_second": ["avg"], "latency_95pctl": ["avg"],},
+                                "filter": {"test_type": "mix"},
+                                "buckets": ["routes", "conn_per_targetroute", "keepalive"],
+                                "aggregations": {"requests_per_second": ["avg"], "latency_95pctl": ["avg"]},
                                 "collate": [],
                             },
                         ],

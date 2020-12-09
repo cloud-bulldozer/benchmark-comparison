d3c5dc43fb1aa34d199e33372473e599299f2877
Amit Sagtani <asagtani06@gmail.com>
2020-12-09 22:34:24 +0530

    added scale data comparison (#33)

---
+++ src/touchstone/benchmarks/scaledata.py
@@ -0,0 +1,80 @@
+import logging
+
+
+from . import BenchmarkBaseClass
+
+
+_logger = logging.getLogger("touchstone")
+
+
+class Scaledata(BenchmarkBaseClass):
+
+    def _build_search(self):
+        _logger.debug("Building search array for Scale data")
+        return self._search_dict[self._source_type][self._harness_type]
+
+    def _build_search_metadata(self):
+        return self._search_dict[self._source_type]["metadata"]
+
+    def _build_compare_keys(self):
+        _logger.debug("Building compare map")
+        _temp_dict = {}
+        for index in self._search_map:
+            _temp_dict[index] = self._search_map[index]['compare']
+        return _temp_dict
+
+    def _build_compute(self):
+        _logger.debug("Building compute map")
+        _temp_dict = {}
+        for index in self._search_map:
+            _temp_dict[index] = self._search_map[index]['compute']
+        return _temp_dict
+
+    def __init__(self, source_type=None, harness_type=None):
+        _logger.debug("Initializing Scale data instance")
+        BenchmarkBaseClass.__init__(self, source_type=source_type,
+                                    harness_type=harness_type)
+        self._search_dict = {
+            'elasticsearch': {
+                'metadata': {
+
+                },
+                'ripsaw': {
+                    'openshift-cluster-timings': {
+                        'compare': ['uuid', 'cluster_name', 'action'],
+                        'compute': [{
+                            'filter': {},
+                            'buckets': ['platform.keyword', 'master_count', 'infra_count',
+                                        'init_worker_count', 'action.keyword', 'worker_count'],
+                            'aggregations': {
+                                'duration': ['avg'],
+                            },
+                            'collate': []
+                        }, ],
+                    },
+                },
+            },
+        }
+        self._search_map = self._build_search()
+        self._search_map_metadata = self._build_search_metadata()
+        self._compute_map = self._build_compute()
+        self._compare_map = self._build_compare_keys()
+        _logger.debug("Finished initializing Scale data instance")
+
+    def emit_compute_map(self):
+        _logger.debug("Emitting built compute map ")
+        _logger.info("Compute map is {} in the database \
+                     {}".format(self._compute_map, self._source_type))
+        return self._compute_map
+
+    def emit_compare_map(self):
+        _logger.debug("Emitting built compare map ")
+        _logger.info("compare map is {} in the database \
+                     {}".format(self._compare_map, self._source_type))
+        return self._compare_map
+
+    def emit_indices(self):
+        return self._search_map.keys()
+
+    def emit_metadata_search_map(self):
+        return self._search_map_metadata

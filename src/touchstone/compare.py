251110add4aababde700df442e69f66f04e01921
Ayesha279 <vkayesha@gmail.com>
2021-07-12 17:15:46 -0400

    test1

--- src/touchstone/compare.py
+++ src/touchstone/compare.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import argparse
 import logging
 import json
@@ -22,10 +21,8 @@ logger = logging.getLogger("touchstone")
 
 def parse_args(args):
     """Parse command line parameters
-
     Args:
       args ([str]): command line parameters as list of strings
-
     Returns:
       :obj:`argparse.Namespace`: command line parameters namespace
     """
@@ -109,7 +106,6 @@ def parse_args(args):
 
 def setup_logging(loglevel):
     """Setup basic logging
-
     Args:
       loglevel (int): minimum loglevel for emitting messages
     """
@@ -121,7 +117,6 @@ def setup_logging(loglevel):
 
 def main(args):
     """Main entry point allowing external calls
-
     Args:
       args ([str]): command line parameter list
     """
@@ -193,27 +188,20 @@ def main(args):
                     for extra_h in ["key", args.identifier, "value"]:
                         compute_header.append(extra_h)
 
-                elif "not-aggregated" in compute:
-
-                    result = database_instance.get_timeseries_results(uuid=uuid, compute_map=compute, index=index, identifier=args.identifier,
-                    )
-                    if not result:
+                elif "timeseries" in compute and compute["timeseries"]:
+                    timeseries_result = database_instance.get_timeseries_results(uuid=uuid, compute_map=compute, index=index, identifier=args.identifier)
+                    if not timeseries_result:
                         logger.error(
                             f"Error: Issue capturing results from {args.database} using config {compute}"
                         )
-
-                    #went through elasticsearch file
-
-                    print('elasticsearch complete \n')
-
-                    mergedicts(result, main_json)
-                    mergedicts(result, index_json)
-                    compute_header = []
-                    for key in compute.get("filter", []):
-                        compute_header.append(key.split(".keyword")[0])
-
                 else:
                     logger.error("Not Supported configutation")
+            if timeseries_result:
+                if not args.output or args.output == "json":
+                    output_file.write(json.dumps(timeseries_result, indent=4))
+                if args.output == "yaml":
+                    output_file.write(yaml.dump(timeseries_result, allow_unicode=True))
+                return
             if index_json:
                 row_list = []
                 if args.output == "csv":
@@ -232,6 +220,7 @@ def main(args):
         output_file.write(json.dumps(main_json, indent=4))
     elif args.output == "yaml":
         output_file.write(yaml.dump(main_json, allow_unicode=True))
+    logger.debug("Script ends here")
     if args.tolerancy_rules:
         sys.exit(
             decision_maker.run(
@@ -248,5 +237,3 @@ def render():
 
 if __name__ == "__main__":
     render()
-
-
